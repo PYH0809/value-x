28cf2e95ed9359915f37578879331671a00080e0
yehan <yemoon@xmind.org>
2025-03-09 23:09:48 +0800

    perf: home page rediect

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,17 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl);
-  // const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-  // if (pathname === '/') {
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = '/stock';
-  //   // ?code=600519&tab=qa&pageNum=1&market=SH
-  //   url.searchParams.set('code', '600519');
-  //   url.searchParams.set('tab', 'qa');
-  //   url.searchParams.set('pageNum', '1');
-  //   url.searchParams.set('market', 'SH');
-  //   return NextResponse.redirect(url);
-  // }
+  if (pathname === '/') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/stock';
+    url.searchParams.set('code', '600519');
+    url.searchParams.set('tab', 'qa');
+    url.searchParams.set('pageNum', '1');
+    url.searchParams.set('market', 'SH');
+    return NextResponse.redirect(url);
+  }
 
   return NextResponse.next();
 }
