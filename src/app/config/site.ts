37f92ca6a7e8133554735d09de398f0a65b0c5c5
yehan <yemoon@xmind.org>
2025-03-10 16:32:05 +0800

    deploy(config): url
    指定 vercel 环境下的域名

--- src/app/config/site.ts
+++ src/app/config/site.ts
@@ -2,6 +2,8 @@ const LOCAL_URL = 'http://localhost:3000';
 
 const url =
   process.env.NEXT_PUBLIC_SITE_URL ||
+  (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL &&
+    `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`) ||
   (process.env.NEXT_PUBLIC_VERCEL_URL && `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) ||
   (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL && `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`) ||
   LOCAL_URL;
